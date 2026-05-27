c6310ec35f94fefb3f535865f9e7606aadafd3e5
YeWenxuan64 <yewenxuan3@foxmail.com>
2026-05-27 15:09:48 +0800

    修复：将构造函数中的 pool_mode 参数重命名为 mult_task

--- ai_inferencer.py
+++ ai_inferencer.py
@@ -36,7 +36,7 @@ class EmptyAIInferencer:
 
 
 class AIInferencer:
-    def __init__(self, model_path:str|None, cores:tuple[int]=(0,), pool_mode:bool=True):
+    def __init__(self, model_path:str|None, cores:tuple[int]=(0,), mult_task:bool=False):
         """
         args:
             model_path: 模型文件路径 str: '/path/to/model'
@@ -45,14 +45,14 @@ class AIInferencer:
             cores: 指定使用哪些核心进行推理 tuple: (0,) or any like (0, 1, 2)
             默认使用第一个核心. 若使用多个核心, 则开启多个线程
 
-            pool_mode: 线程池推理模式 bool: True or False
+            mult_task: 线程池推理模式 bool: True or False
             若为True, 则使用线程池进行推理, 否则使用单线程推理器
         """
         self.model_path = str(model_path)
         if isinstance(cores, int):
             cores = tuple([cores])
         self.cores = cores
-        self.pool_mode = pool_mode
+        self.mult_task = mult_task
 
         self.inferfacer = EmptyAIInferencer()
         self.inferfacer_init()
@@ -87,7 +87,7 @@ class AIInferencer:
             with temporary_sys_path(CURRENT_DIR):
                 import rknn_inferencer as rknn_infer
 
-            if self.pool_mode is True:
+            if self.mult_task:
                 self.inferfacer = rknn_infer.RknnThreadPool(self.model_path, self.cores)
             else:
                 self.inferfacer = rknn_infer.RknnExecutor(self.model_path, self.cores)
@@ -96,7 +96,7 @@ class AIInferencer:
             with temporary_sys_path(CURRENT_DIR):
                 import qnn_inferencer as qnn_infer
 
-            if self.pool_mode is True:
+            if self.mult_task:
                 self.inferfacer = qnn_infer.QnnProcessPool(self.model_path, self.cores)
             else:
                 self.inferfacer = qnn_infer.QnnExecutor(self.model_path)
@@ -110,3 +110,33 @@ class AIInferencer:
     def release(self) -> bool:
         ret = self.inferfacer.release()
         return ret
+    
+
+
+
+def timeit(func):
+    import time
+    from functools import wraps
+    from collections import deque
+    time_length = 30
+    time_list = deque(maxlen=time_length)
+    last_print_time = time.perf_counter()
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        nonlocal last_print_time
+
+        start = time.perf_counter()
+        try:
+            result = func(*args, **kwargs)
+        finally:
+            end = time.perf_counter()
+            time_list.append(end - start)
+
+            if end - last_print_time >= 1.0:
+                last_print_time = end
+                mean_time = sum(time_list) / time_length
+                print(f"{func.__name__} : {mean_time*1000:.3f} ms")
+
+        return result
+    return wrapper
